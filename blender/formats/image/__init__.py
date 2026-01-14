f41e9c701c626dd974708ac107c3436240ecc0f1
unwave <39925416+unwave@users.noreply.github.com>
2026-01-14 22:43:29 +0300

    make _generated.py optional for formats.image

--- blender/formats/image/__init__.py
+++ blender/formats/image/__init__.py
@@ -1,12 +1,35 @@
 import typing
+import os
 
 
 if __spec__.name == __name__:
     from blend_converter.blender.formats.image.export_image import export_image
-    from blend_converter.blender.formats.image._generated import Settings_Image, Settings_Cycles, Settings_Eevee, Settings_Render, Settings_View
 else:
     from .export_image import export_image
-    from ._generated import Settings_Image, Settings_Cycles, Settings_Eevee, Settings_Render, Settings_View
+
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+if not os.path.exists(os.path.join(DIR, '_generated.py')):
+
+    if __spec__.name == __name__:
+        from blend_converter.blender.formats.image._generated import Settings_Image, Settings_Cycles, Settings_Eevee, Settings_Render, Settings_View
+    else:
+        from ._generated import Settings_Image, Settings_Cycles, Settings_Eevee, Settings_Render, Settings_View
+
+elif not typing.TYPE_CHECKING:
+
+    if __spec__.name == __name__:
+        from blend_converter import tool_settings
+    else:
+        from .... import tool_settings
+
+    class Fake_Settings(tool_settings.Settings):
+
+        allow_missing_settings = True
+
+    Settings_Image = Settings_Cycles = Settings_Eevee = Settings_Render = Settings_View = Fake_Settings
 
 
 
