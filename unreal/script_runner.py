158f749aaf1a335270511aefd93e16ec7e5fda14
unwave <39925416+unwave@users.noreply.github.com>
2026-02-04 15:54:49 +0300

    fix the unreal script runner's path to blend_converter

--- unreal/script_runner.py
+++ unreal/script_runner.py
@@ -7,7 +7,7 @@ import sys
 import typing
 
 
-BLEND_CONVERTER_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+BLEND_CONVERTER_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 BLEND_CONVERTER_INIT_PY = os.path.join(BLEND_CONVERTER_ROOT_DIR, '__init__.py')
 
 
