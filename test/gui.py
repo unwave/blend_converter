6c9954281065325d9e489c46696f5ad92659e9b2
unwave <39925416+unwave@users.noreply.github.com>
2026-03-01 05:24:55 +0300

    for parallel execution use only physical cores for system responsiveness

--- test/gui.py
+++ test/gui.py
@@ -5,11 +5,8 @@ def main():
 
     app = updater_ui.Main_Frame.get_app([common.Program_Definition(__file__, 'get_bake_program', 'get_keyword_arguments')])
 
-    import os
-    cpu_count = os.cpu_count()
-
-    # import psutil
-    # cpu_count = psutil.cpu_count(logical=False)
+    import psutil
+    cpu_count = psutil.cpu_count(logical=False)
 
     app.main_frame.updater.total_max_parallel_executions = cpu_count
     app.main_frame.updater.default_max_parallel_executions = cpu_count
