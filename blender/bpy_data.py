dd2e820bf757ef5df1c558b6fd41eacce9cc1787
unwave <39925416+unwave@users.noreply.github.com>
2026-04-09 14:00:03 +0300

    data: refactor loading a block for generic use

--- blender/bpy_data.py
+++ blender/bpy_data.py
@@ -20,24 +20,28 @@ elif not typing.TYPE_CHECKING:
     bpy = utils.Dummy()
 
 
+def load_block(filepath: str, type: str, name: str):
+
+    block = getattr(bpy.data, type).get(name)
+    if block:
+        return block
+
+    with bpy.data.libraries.load(filepath) as (data_from, data_to):
+        setattr(data_to, type, [name])
+
+    block = getattr(data_to, type)[0]
+
+    if not block:
+        raise Exception(f"Fail to load: {name} from {filepath}")
+
+    return block
+
 
 def load_compositor_node_tree(name: str) -> bpy.types.CompositorNodeTree:
 
     data_file = os.path.join(common.ROOT_DIR, 'blender', 'blends', 'compositor_node_groups_2_93.blend')
 
-    node_group = bpy.data.node_groups.get(name)
-    if node_group:
-        return node_group
-
-    with bpy.data.libraries.load(data_file) as (data_from, data_to):
-        data_to.node_groups = [name]
-
-    node_group = data_to.node_groups[0]
-
-    if not node_group:
-        raise Exception(f"Fail to load: {name}")
-
-    return node_group
+    return load_block(data_file, 'node_groups', name)
 
 
 def open_mainfile(filepath: str, load_ui = True, use_scripts = False):
